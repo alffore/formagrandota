b0e9ec1498d0b03cb1f57692d59fcbc27ec3e0e6
A. Alfonso F. R <alffore@gmail.com>
2020-06-05 14:02:45 -0500

    Proyecto Tabla

---
+++ src/proyecto1/index2.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <style>
+
+        #hojaceldas{
+            min-width: 500px;
+            width: 50%;
+            max-width: 900px;
+            margin-left: auto;
+            margin-right: auto;
+            border: 1px solid #ccc;
+
+        }
+
+        .renglon{
+            width: 100%;
+            height: 30px;
+           /* border: 1px solid #f00;*/
+            display: flex;
+        }
+
+        .celda{
+            min-width: 10%;
+            height: 30px;
+            /*border: 1px solid #0f0;*/
+        }
+
+    </style>
+
+</head>
+<body>
+    <div id="hojaceldas">    
+    <?php
+        $ncx = 10;
+        $ncy =5;
+
+        $buff="";
+        for($j=0;$j<$ncy;$j++){
+            $buff.='<div class="renglon">';
+
+            for($i=0;$i<$ncx;$i++){
+                $buff.='<div class="celda">';
+                $buff.='<input type="text" name="" id="" value="">';    
+                $buff.='</div>';
+            }
+
+            $buff.='</div>';
+        }
+
+        echo $buff;
+    ?>
+    </div>
+</body>
+</html>
