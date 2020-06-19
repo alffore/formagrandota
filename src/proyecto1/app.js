f8ed6a97b1660a8e30f87fa61ef79e7516054d81
A. Alfonso F. R <alffore@gmail.com>
2020-06-19 14:09:53 -0500

    Se crea un selector dinamico via JS

--- src/proyecto1/app.js
+++ src/proyecto1/app.js
@@ -8,6 +8,8 @@ aobjselec.forEach(e => {
 
     e.addEventListener('focus',evt => {
         console.log(`foco: ${evt.target.id}`)
+        console.log(evt)
+        generaSelector2(evt.target)
     })
 
     e.addEventListener('blur',evt =>{
@@ -23,4 +25,21 @@ function generaSelector(valor){
     avselec.forEach(v =>{
         buffer+='<option value="">'
     })    
+}
+
+
+function generaSelector2(obj){
+
+    var nuevoSelector = document.createElement("select");
+
+    let i=0
+    avselec.forEach(op => { 
+        nuevoSelector[i++]=new Option(op);
+
+    })
+
+    obj.parentElement.appendChild(nuevoSelector)
+    let valor = obj.value
+    console.log(valor)
+    obj.remove()
 }
